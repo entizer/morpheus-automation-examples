a5425bad17aaf70589f2a00676ac76c5465a3ed4
Korey Gawronski <kg-tech@hotmail.com>
2023-04-25 16:55:04 -0400

    tabbed

--- Terraform/AWS/simpleS3/main.tf
+++ Terraform/AWS/simpleS3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "s3" {
     bucket_prefix = var.bucket_prefix
     force_destroy = var.force_destroy
     tags = {
-    Name = var.bucket_prefix
+      Name = var.bucket_prefix
     }
 }
 
